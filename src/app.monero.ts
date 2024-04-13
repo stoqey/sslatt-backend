e07ae030789fdba6753ee59d609b762df6a18a7f
ceddybi <ceddymuhoza@gmail.com>
2024-04-13 15:12:01 -0400

    update siteConfigSetEnv

---
+++ src/app.monero.ts
@@ -0,0 +1,40 @@
+// custom roadman 
+import 'cross-fetch/polyfill';
+import "reflect-metadata";
+import "dotenv/config";
+
+import { Query, Resolver } from 'type-graphql';
+import { RoadmanBuild, roadman, log } from "roadman";
+import { walletRouter as moneroxWalletRouter, listenMain } from "@roadmanjs/monerox";
+
+@Resolver()
+class ExampleResolver {
+    @Query(() => [String], { nullable: true })
+    async apps(): Promise<string[]> {
+        // fake async in this example
+        return ['Apps', 'one', 'two'];
+    }
+}
+
+const customRoadmanApp = async (args: RoadmanBuild): Promise<RoadmanBuild> => {
+    const { app } = args;
+    app.use("/wallet", moneroxWalletRouter());
+    return args;
+}
+
+const run = async () => {
+
+    const roadmanStarted = await roadman({
+        roadmen: [
+            customRoadmanApp
+        ],
+        resolvers: [ExampleResolver]
+    });
+
+    if(roadmanStarted){
+        await listenMain();
+        log("roadmanStarted", roadmanStarted);
+    }
+};
+
+run();
--- src/monerox.app.ts
+++ src/monerox.app.ts
@@ -1,43 +1,13 @@
-// custom roadman 
 import 'cross-fetch/polyfill';
 import "reflect-metadata";
 import "dotenv/config";
 
-import { Query, Resolver } from 'type-graphql';
-import { RoadmanBuild, roadman, log } from "roadman";
-import { walletRouter as moneroxWalletRouter, listenMain } from "@roadmanjs/monerox";
-import { siteConfigSetEnv } from "./settings/settings.methods";
-
-@Resolver()
-class ExampleResolver {
-    @Query(() => [String], { nullable: true })
-    async apps(): Promise<string[]> {
-        // fake async in this example
-        return ['Apps', 'one', 'two'];
-    }
-}
-
-const customRoadmanApp = async (args: RoadmanBuild): Promise<RoadmanBuild> => {
-    const { app } = args;
-    app.use("/wallet", moneroxWalletRouter());
-    return args;
-}
+import { get as _get } from "lodash";
+import { siteConfigSetEnv } from './settings/settings.methods';
 
 const run = async () => {
-
-    await siteConfigSetEnv();
-
-    const roadmanStarted = await roadman({
-        roadmen: [
-            customRoadmanApp
-        ],
-        resolvers: [ExampleResolver]
-    });
-
-    if(roadmanStarted){
-        await listenMain();
-        log("roadmanStarted", roadmanStarted);
-    }
+  await siteConfigSetEnv();
+  await import("./app.monero");
 };
 
 run();
